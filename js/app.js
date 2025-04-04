3a5dead584459932f8ecc77234be18bfbe6fd608
Gabriel-Tremblay <159824960+Gabriel-Tremblay@users.noreply.github.com>
2025-04-04 11:53:14 -0400

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,24 @@
+fetch('https://api.ipify.org?format=json')
+    .then(response => response.json())
+    .then(data => {
+      const ipAddress = data.ip;
+
+      document.getElementById('ip').innerText = "Adresse IP: " + ipAddress
+
+      fetch(`https://api.country.is/${ipAddress}`)
+        .then(response => response.json())
+        .then(location => {
+          document.getElementById('country').innerText = "Pays du visiteur: " + location.country;
+          if(location.country !== "CA") {
+            document.location.href="https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+          }
+        })
+        .catch(error => {
+          console.error('Erreur lors de la récupération du pays:', error);
+          document.getElementById('country').innerText = "Incapable de récupérer le pays"
+        });
+    })
+    .catch(error => {
+      console.error('Erreur lors de la récupération de l\'adresse ip:', error);
+      document.getElementById('id').innerText = "Incapable de récupérer l'adresse ip"
+    })
